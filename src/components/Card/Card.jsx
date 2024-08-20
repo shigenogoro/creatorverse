16bf518f9541ccdaa034686497766b09ce55525e
shigenogoro <kyle990987@gmail.com>
2024-08-20 15:12:34 -0400

    Modified the Opacity of Background Image without affecting the Text

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,23 +2,27 @@ import './Card.css'
 
 const Card = ({name, ytURL, twitterURL, igURL, intro, imgURL}) => {
     return (
-        <div className="card" style={{backgroundImage: `url(${imgURL})`}}>
-            <h3>{name}</h3>
-            <div className="info-group">
-                <div className="btn-group">
-                    <div className="social-btn-group">
-                        <a href={ytURL} className="fa fa-youtube-play"></a>
-                        <a href={twitterURL} className="fa fa-twitter"></a>
-                        <a href={igURL} className="fa fa-instagram"></a>
+        <div className="card">
+            <div className="card-bg" style={{backgroundImage: `url(${imgURL})`}}></div>
+
+            <div className="card-content">
+                <h3>{name}</h3>
+                <div className="info-group">
+                    <div className="btn-group">
+                        <div className="social-btn-group">
+                            <a href={ytURL} className="fa fa-youtube-play"></a>
+                            <a href={twitterURL} className="fa fa-twitter"></a>
+                            <a href={igURL} className="fa fa-instagram"></a>
+                        </div>
+                        <div className="info-btn-group">
+                            <a href="#" className="fa fa-info-circle"></a>
+                            <a href="#" className="fa fa-edit"></a>
+                        </div>
                     </div>
-                    <div className="info-btn-group">
-                        <a href="#" className="fa fa-info-circle"></a>
-                        <a href="#" className="fa fa-edit"></a>
-                    </div>
+                    <p className="description-box">
+                        {intro}
+                    </p>
                 </div>
-                <p className="description-box">
-                    {intro}
-                </p>
             </div>
         </div>
     )
