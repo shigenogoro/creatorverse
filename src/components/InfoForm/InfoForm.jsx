8525e28187d63f99f0e454d2eb838bf61eaa1979
shigenogoro <kyle990987@gmail.com>
2024-08-21 11:55:10 -0400

    Bundle InfoForm from Page - Skeleton

---
+++ src/components/InfoForm/InfoForm.jsx
@@ -0,0 +1,101 @@
+const InfoForm = ({values, onValuesChange}) => {    
+    // Callback for lifting change to the parent component.
+    const handleChange = (field) => (event) => {
+        const value = event.target.value;
+        onValuesChange(field, value); // Call the parent's callback with the updated field and value
+    }
+
+
+    return (
+        <div className="form-container">
+            <div className="info-container">
+                <form>
+                    <legend>Personal Information</legend>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <br />
+                        <input 
+                            onChange={handleChange('name')}
+                            type="text" 
+                            name="name"  
+                            id="name" 
+                            value={values.name || ''}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="imgURL">Image</label>
+                        <br />
+                        <input 
+                            onChange={handleChange('imgURL')}
+                            type="text" 
+                            name="imgURL"  
+                            id="imgURL" 
+                            value={values.imgURL || ''}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <br />
+                        <input 
+                            onChange={handleChange('description')}
+                            type="textarea" 
+                            name="description"  
+                            id="description" 
+                            value={values.description || ''}
+                        />
+                    </div>
+                </form>
+            </div>
+            <div className="social-container">
+                    <form>
+                        <legend>Social Media Link</legend>
+                        <div>
+                            <label htmlFor="ytURL">
+                                <span><i className="fa fa-youtube-play"></i> YouTube</span>
+                            </label>
+                            <br />
+                            <input 
+                                onChange={handleChange('ytURL')}
+                                type="text" 
+                                name="ytURL"  
+                                id="ytURL" 
+                                value={values.ytURL || ''}
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="twitterURL">
+                                <span><i className="fa fa-twitter"></i>Twitter</span>
+                            </label>
+                            <br />
+                            <input 
+                                onChange={handleChange('twitterURL')}
+                                type="text" 
+                                name="twitterURL"  
+                                id="twitterURL" 
+                                value={values.twitterURL|| ''}
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="igURL">
+                                <span><i className="fa fa-instagram"></i>Instagram</span>
+                            </label>
+                            <br />
+                            <input 
+                                onChange={handleChange('igURL')}
+                                type="text" 
+                                name="igURL"  
+                                id="igURL" 
+                                value={values.igURL || ''}
+                            />
+                        </div>
+                    </form>
+                </div>
+        </div>
+    )
+}
+
+export default InfoForm;
--- src/pages/EditCreator/EditCreator.jsx
+++ src/pages/EditCreator/EditCreator.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { useParams, Navigate } from "react-router-dom";
 import MyButton from "../../components/MyButton/MyButton";
+import InfoForm from "../../components/InfoForm/InfoForm";
 import './EditCreator.css'
 import { supabase } from "../../client";
 
@@ -24,14 +25,6 @@ const EditCreator = () => {
         setValues(data);
     }
 
-    const onValuesChange = (field) => (event) => {
-        setValues(prevValues => {
-            const updatedValues = [...prevValues]; // Copy the previous array
-            updatedValues[0] = { ...updatedValues[0], [field]: event.target.value }; // Update the first element
-            return updatedValues;
-        });
-    }
-
     const onButtonSubmit = async () => {
         const { error } = await supabase.from('creators').update(values[0]).eq('id', creatorId.id)
         if(error) {
@@ -46,6 +39,15 @@ const EditCreator = () => {
         setRedirect(true);
     }
 
+    // Callback Method: Get data from Child to Parent
+    const handleValuesChange = (field, value) => {
+        setValues(prevValues => {
+            const updatedValues = [...prevValues]; 
+            updatedValues[0] = { ...updatedValues[0], [field]: value }; 
+            return updatedValues;
+        });
+    }
+
     if(redirect) {
         return (
             <Navigate to={'/'} />
@@ -54,93 +56,8 @@ const EditCreator = () => {
 
     return (
         <div className="edit-container">
-            <h2 className="title">Personal Info</h2>
-            <div className="info-container">
-                <form>
-                    <div>
-                        <label htmlFor="name">Name</label>
-                        <br />
-                        <input 
-                            onChange={onValuesChange('name')}
-                            type="text" 
-                            name="name"  
-                            id="name" 
-                            value={values[0].name ? values[0].name : ''}
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="imgURL">Image</label>
-                        <br />
-                        <input 
-                            onChange={onValuesChange('imgURL')}
-                            type="text" 
-                            name="imgURL"  
-                            id="imgURL" 
-                            value={values[0].imgURL ? values[0].imgURL : ''}
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="description">Description</label>
-                        <br />
-                        <input 
-                            onChange={onValuesChange('description')}
-                            type="textarea" 
-                            name="description"  
-                            id="description" 
-                            value={values[0].description ? values[0].description : ''}
-                        />
-                    </div>
-                </form>
-            </div>
-
-            <h2 className="title">Social Media Link</h2>
-            <div className="social-container">
-                <form>
-                    <div>
-                        <label htmlFor="ytURL">
-                            <span><i className="fa fa-youtube-play"></i> YouTube</span>
-                        </label>
-                        <br />
-                        <input 
-                            onChange={onValuesChange('ytURL')}
-                            type="text" 
-                            name="ytURL"  
-                            id="ytURL" 
-                            value={values[0].ytURL ? values[0].ytURL : ''}
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="twitterURL">
-                            <span><i className="fa fa-twitter"></i>Twitter</span>
-                        </label>
-                        <br />
-                        <input 
-                            onChange={onValuesChange('twitterURL')}
-                            type="text" 
-                            name="twitterURL"  
-                            id="twitterURL" 
-                            value={values[0].twitterURL ? values[0].twitterURL : ''}
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="igURL">
-                            <span><i className="fa fa-instagram"></i>Instagram</span>
-                        </label>
-                        <br />
-                        <input 
-                            onChange={onValuesChange('igURL')}
-                            type="text" 
-                            name="igURL"  
-                            id="igURL" 
-                            value={values[0].igURL ? values[0].igURL : ''}
-                        />
-                    </div>
-                </form>
-            </div>
+            
+            <InfoForm values={values[0]} onValuesChange={handleValuesChange}  />
 
             <div className="btn-group">
                 <MyButton title="Submit" type="normal" onClick={onButtonSubmit} />
