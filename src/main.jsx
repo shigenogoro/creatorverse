6662539d0beca23f7047a542715f4741b4ec9f71
shigenogoro <kyle990987@gmail.com>
2024-08-21 22:29:51 -0400

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import MainRouter from './MainRouter.jsx'
+import { Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import ViewCreator from './pages/ViewCreator/ViewCreator.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <App />
-      <MainRouter />
+      <Routes>
+        {/* Route for the home page */}
+        <Route path="/" element={<App />} />
+
+        {/* Route for the contact page */}
+        <Route path='/view/:id' element={<ViewCreator />} />
+
+        {/* Catch-all route in case of unmatched paths */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
     </HashRouter>
   </StrictMode>,
 )
