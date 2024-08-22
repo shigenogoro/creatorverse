24ea295315dad08aeb3e1e2a7c3e1e40bb9e3680
shigenogoro <kyle990987@gmail.com>
2024-08-21 21:25:21 -0400

    fixed the routing for publish

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,14 +1,9 @@
 import { useRoutes } from 'react-router-dom';
-import App from './App';
 import ViewCreator from './pages/ViewCreator/ViewCreator';
 
 const MainRouter = () => {
     // Routing
     const routes = useRoutes([
-        { 
-            path: '/', 
-            element: <App />
-        },
         {
             path: '/view/:id',
             element: <ViewCreator />
